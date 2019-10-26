b11ff4a71b6938f19bb842348af3d6145b47d4d5
naser01 <nnkilani@live.com>
2019-10-26 18:07:04 +0200

    10

--- JS/script.js
+++ JS/script.js
@@ -1,17 +1,19 @@
 function buttons() {
 
-    eq= [];
-
     document.getElementById('1').addEventListener('click', e => {
         eq.push(1);
+        const markup = `<p>1<p>`;
+        
     });
 
     document.getElementById('2').addEventListener('click', e => {
         eq.push(2);
+        const markup = `<p>2<p>`;
     });
 
     document.getElementById('3').addEventListener('click', e => {
         eq.push(3);
+        const markup = `<p>3<p>`;
     });
 
     document.getElementById('4').addEventListener('click', e => {
@@ -39,7 +41,7 @@ function buttons() {
     });
 
     document.getElementById('plus').addEventListener('click', e => {
-
+        eq.push('+'); 
     });
 
     document.getElementById('minus').addEventListener('click', e => {
@@ -53,12 +55,17 @@ function buttons() {
     document.getElementById('div').addEventListener('click', e => {
 
     });
-
-    console.log(eq);
-    eq.forEach(element => {
-        console.log(element);
-    });
+    
 }
+
+let eq = [];
+
+let h = eq.map( x => {
+    x.toString();
+}) 
+
+console.log(h);
+
 
 buttons();
 
