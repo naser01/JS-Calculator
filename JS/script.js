aae71ec112736647aafb780d0a1240b5835d1c5d
naser01 <nnkilani@live.com>
2019-10-27 17:36:36 +0200

    12

--- JS/script.js
+++ JS/script.js
@@ -1,73 +1,110 @@
 /*import { elements } from "./base.js";*/
 
+let eq = [];
+
 function buttons() {
 
-    document.getElementById('1').addEventListener('click', e => {
-        eq.push(1);
+    document.getElementById('zero').addEventListener('click', e => {
+        eq.push('0');
+        document.querySelector('.terminal-nums').insertAdjacentText('beforeend', 0);
+    });
 
-        document.querySelector('.terminal-eq').insertAdjacentText('beforeend', 1);
+    document.getElementById('1').addEventListener('click', e => {
+        eq.push('1');
+
+        document.querySelector('.terminal-nums').insertAdjacentText('beforeend', 1);
     });
 
     document.getElementById('2').addEventListener('click', e => {
-        eq.push(2);
-        document.querySelector('.terminal-eq').insertAdjacentText('beforeend', 2);
+        eq.push('2');
+        document.querySelector('.terminal-nums').insertAdjacentText('beforeend', 2);
     });
 
     document.getElementById('3').addEventListener('click', e => {
-        eq.push(3);
-        document.querySelector('.terminal-eq').insertAdjacentText('beforeend', 3);
+        eq.push('3');
+        document.querySelector('.terminal-nums').insertAdjacentText('beforeend', 3);
     });
 
     document.getElementById('4').addEventListener('click', e => {
-
+        eq.push('4');
+        document.querySelector('.terminal-nums').insertAdjacentText('beforeend', 4);
     });
 
     document.getElementById('5').addEventListener('click', e => {
-
+        eq.push('5');
+        document.querySelector('.terminal-nums').insertAdjacentText('beforeend', 5);
     });
 
     document.getElementById('6').addEventListener('click', e => {
-
+        eq.push('6');
+        document.querySelector('.terminal-nums').insertAdjacentText('beforeend', 6);
     });
 
     document.getElementById('7').addEventListener('click', e => {
-
+        eq.push('7');
+        document.querySelector('.terminal-nums').insertAdjacentText('beforeend', 7);
     });
 
     document.getElementById('8').addEventListener('click', e => {
-
+        eq.push('8');
+        document.querySelector('.terminal-nums').insertAdjacentText('beforeend', 8);
     });
 
     document.getElementById('9').addEventListener('click', e => {
-
+        eq.push('9');
+        document.querySelector('.terminal-nums').insertAdjacentText('beforeend', 9);
     });
 
     document.getElementById('plus').addEventListener('click', e => {
-        document.querySelector('.terminal-eq').insertAdjacentText('beforeend', '+');
+        eq.push('+');
+        document.querySelector('.terminal-nums').insertAdjacentText('beforeend', '+');
     });
 
     document.getElementById('minus').addEventListener('click', e => {
-
+        eq.push('-');
+        document.querySelector('.terminal-nums').insertAdjacentText('beforeend', '-');
     });
 
     document.getElementById('multi').addEventListener('click', e => {
-
+        eq.push('*');
+        document.querySelector('.terminal-nums').insertAdjacentText('beforeend', '*');
     });
 
     document.getElementById('div').addEventListener('click', e => {
+        eq.push('/');
+        document.querySelector('.terminal-nums').insertAdjacentText('beforeend', '/');
+    });
 
+    document.getElementById('brac-left').addEventListener('click', e => {
+        eq.push('(');
+        document.querySelector('.terminal-nums').insertAdjacentText('beforeend', '(');
+    });
+
+    document.getElementById('brac-right').addEventListener('click', e => {
+        eq.push('/');
+        document.querySelector('.terminal-nums').insertAdjacentText('beforeend', ')');
     });
     
 }
 
-let eq = [];
-
-let h = eq.map( x => {
-    x.toString();
-}) 
-
-console.log(h);
+function result(){
+    function calc(h){
+        const eqString = h.join('');
+        const evaluatString = eval(eqString);
+        console.log(evaluatString);    
+        return(evaluatString) ;
+    }
+    document.getElementById('equal').addEventListener('click', e => {
+        document.querySelector('.terminal-result').insertAdjacentHTML('beforeend', calc(eq));
+    })  
+}
 
 
 buttons();
+result();
+
+
+
+
+
 
